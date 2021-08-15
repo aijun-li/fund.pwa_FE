da4799b4da7f29eeb007ca5426cea4a6a553030f
Aijun Li <liaijun.cn@gmail.com>
2021-08-15 15:53:03 +0800

    feat: preliminary PWA support

---
+++ pwa.config.ts
@@ -0,0 +1,58 @@
+import { Options } from 'vite-plugin-pwa'
+
+const userOptions: Partial<Options> = {
+  strategies: 'generateSW',
+  registerType: 'autoUpdate',
+  injectRegister: 'script',
+  includeAssets: ['favicon.svg', 'robots.txt'],
+  manifest: {
+    name: 'Fund.PWA',
+    short_name: 'Fund.PWA',
+    theme_color: '#ffffff',
+    description: 'A simple fund application',
+    icons: [
+      {
+        src: 'pwa-48x48.png',
+        sizes: '48x48',
+        type: 'image/png'
+      },
+      {
+        src: 'pwa-96x96.png',
+        sizes: '96x96',
+        type: 'image/png'
+      },
+      {
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      },
+      {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable'
+      }
+    ]
+  },
+  workbox: {
+    runtimeCaching: [
+      {
+        urlPattern: /^https:\/\/fund.bytecask.com\/api\/.*/i,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'site-api-cache',
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      }
+    ]
+  }
+}
+
+export default userOptions
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import path from 'path'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import pwaOptions from './pwa.config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [reactRefresh(), VitePWA(pwaOptions)],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
